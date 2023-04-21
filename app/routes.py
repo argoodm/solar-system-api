946abfd578e59ce83bc1a1d209f32d2740694737
Abby <argoodm@gmail.com>
2023-04-21 11:23:00 -0400

    class definition

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
 from flask import Blueprint
 
+
+class Planets:
+    def __init__(id, name, description, has_flag, has_moon):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.has_flag = False
+
