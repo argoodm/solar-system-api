3872bb3fd33ed8e9a5261e28fa42e705f5691155
Abby <argoodm@gmail.com>
2023-04-21 11:38:05 -0400

    list of planets

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,18 @@ from flask import Blueprint
 
 
 class Planets:
-    def __init__(id, name, description, has_flag, has_moon):
+    def __init__(id, name, description, has_flag= False):
         self.id = id
         self.name = name
         self.description = description
-        self.has_flag = False
+        self.has_flag = has_flag
 
+planets = [
+    Planets(name, description),
+    Planets(name, description),
+    Planets(name, description),
+    Planets(nname, description),
+    Planets(name, description),
+
+
+]
