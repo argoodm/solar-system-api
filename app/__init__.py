15bd6d80477ff0c687837029337780c88a15cd33
Stacey <stacey.l.joseph@gmail.com>
2023-04-23 08:37:48 -0400

    initial GET routes successful

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,13 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    
+    # Registering Blueprints
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+    
+    from .routes import moons_bp
+    app.register_blueprint(moons_bp)
+
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
 from flask import Blueprint
 
+# Creating the Blueprints 
+planets_bp = Blueprint("planets", __name__)
+moons_bp = Blueprint("moons", __name__)
+
+# Defining READ Routes with GET method
+@planets_bp.route("/planets", methods=["GET"])
+def endpoint_name():
+    response_body = "Hello, Planets!"
+    return response_body
+
+@moons_bp.route("/moons", methods=["GET"])
+def endpoint_name():
+    response_body = "Hello, Moons!"
+    return response_body
+
 
 class Planets:
     def __init__(id, name, description, has_flag= False):
@@ -8,12 +23,10 @@ class Planets:
         self.description = description
         self.has_flag = has_flag
 
-planets = [
-    Planets(name, description),
-    Planets(name, description),
-    Planets(name, description),
-    Planets(nname, description),
-    Planets(name, description),
-
-
-]
+# planets = [
+#     Planets(name, description),
+#     Planets(name, description),
+#     Planets(name, description),
+#     Planets(nname, description),
+#     Planets(name, description),
+# ]
