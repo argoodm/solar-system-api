d390ccb9fb6a9a8a2cf398b7302668ac94001b0c
Abby <argoodm@gmail.com>
2023-04-28 12:03:15 -0400

    added init route and model changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sql_alchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-
+db = SQLAlchemy()
+migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
     
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
     # Import Routes & Register Blueprints
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,38 +1,21 @@
-from flask import Blueprint
-from flask import jsonify
-
-# Define a `Planet` class with attributes:
-# `id`, `name`,`description`, and one additional(has_flag)
-class Planet:
-    def __init__(self, id, name, description, has_flag= False):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.has_flag = has_flag
-
-# Create a list of `Planet` instances
-# (ordered nearest to the sun to the furthest)
-planets = [
-    Planet(1, "Mercury", "The closest to the sun. Rocky terrestrial planet."),
-    Planet(2, "Venus", "2nd closest to the sun. Rocky terrestrial planet."),
-    Planet(3, "Earth", "3rd closest to the sun. Rocky terrestrial planet."),
-    Planet(4, "Mars", "4th closest to the sun. Rocky terrestrial planet."),
-    Planet(5, "Jupiter", "5th closest to the sun. Jovian planet, one of the gas giants."),
-    Planet(6, "Saturn", "6th closest to the sun. Jovian planet, one of the gas giants."),
-    Planet(7, "Uranus", "7th closest to the sun. Jovian planet,one of the ice giants."),
-    Planet(8, "Neptune", "8th closest to the sun. Jovian planet, one of the ice giants."),
-    Planet(9, "Pluto", "9th closets to the sun. A controversial dwarf planet, with wildly tilted, elliptical orbit"),
-]
+from flask import Blueprint, jsonify, request
+from app import db
+from .models.planet import Planets
 
 
-# Creating the Planet Blueprint 
-# planets_bp = Blueprint("planets", __name__)
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
-
+@planets_bp.route("", methods=['POST'])
 
 # Defining READ Routes with GET method for Planets
 # @planets_bp.route("/planets", methods=["GET"])
 @planets_bp.route("", methods=["GET"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planets(
+        name = request_body["name"],
+        description= request_body["description"],
+        has_flag= request_body["has_flag"]
+    )
 
 def get_planets_endpoint():
     planet_response_body = []
@@ -50,6 +33,10 @@ def get_planets_endpoint():
 
 
 # ================================== WAVE 2 =========================
+#helper function
+
+
+
 # get one planet, example: http://localhost:5000/planets/2:
 @planets_bp.route("/<planet_id>", methods = ["GET"])
 def get_planet(planet_id):
@@ -71,11 +58,6 @@ def get_planet(planet_id):
             }
     # handles non-existing planet id numbers, like http://localhost:5000/planets/12 : 
     return {"message":f"planet {planet_id} not found"}, 404
-
-
-
-
-
 
 
 # Creating the Moon Blueprint 
