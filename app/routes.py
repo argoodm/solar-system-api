29c004222a94479398962bcbdf26189807aec77b
Stacey <stacey.l.joseph@gmail.com>
2023-05-02 11:25:10 -0400

    adds validate function

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,34 @@ from flask import Blueprint, jsonify, request, make_response, abort
 from app import db
 from .models.planet import Planet
 
-# ============================= CREATE ============================= 
+# ================================== HELPER FUNCTION =========================
+# validate planet helper function
+# responsible for validating & returning the instance of the planet
+#  NOT REFACTORED
+def validate_planet(planet_id):
+    # handle invalid planet_id, return 400
+    try: 
+        planet_id = int(planet_id)
+        
+    except: 
+        abort(make_response(return {"message":f"planet id {planet_id}  is invalid"}, 400)) 
+
+    # search for planet_id in data, return planet
+    for planet in planets :
+        if planet.id == planetid:
+            return planet
+            
+
+    # return a 404 for non-existing 
+    abort(make_response(return {"message" : f"book {book_id} not found"}, 404))
+
+
+# ================================== BLUEPRINT =========================
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+# ============================= CREATE ============================= 
+# route for creating a planet
 @planets_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
@@ -20,10 +46,10 @@ def create_planet():
 
 
 # ============================= READ ============================= 
-# Defining READ Routes with GET method for Planets
+# route with GET method to read all planets
 # @planets_bp.route("/planets", methods=["GET"])
 @planets_bp.route("", methods=["GET"])
-def get_planets_endpoint():
+def get_all_planets():
     planet_response_body = []
     
     for planet in planets:
@@ -36,11 +62,11 @@ def get_planets_endpoint():
     return jsonify(planet_response_body)
 
 
-#helper function 
-
+# route with GET method to read one planet
+# @planets_bp.route("/planets", methods=["GET"])
 # get one planet, example: http://localhost:5000/planets/2:
 @planets_bp.route("/<planet_id>", methods = ["GET"])
-def get_planet(planet_id):
+def get_one_planet(planet_id):
     # planet_id = int(planet_id)
     
     # placing the above line within a try/ except block
@@ -60,6 +86,11 @@ def get_planet(planet_id):
     # handles non-existing planet id numbers, like http://localhost:5000/planets/12 : 
     return {"message":f"planet {planet_id} not found"}, 404
 
+# ============================= UPDATE ============================= 
+
+
+
+# ============================= DELETE ============================= 
 
 
 
@@ -71,4 +102,5 @@ def get_planet(planet_id):
 # def endpoint_name():
 #     response_body = "Hello, Moons!"
 #     return response_body
+
 
